31b32007788ed8f4c63f25bf14cec914b969b11b
Vitalii Soloninko <130390124+VitaliiSoloninko@users.noreply.github.com>
2025-12-26 09:48:17 +0100

    feat(new page): generate new page, add button back

---
+++ src/app/new/new.page.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonButtons,
+  IonBackButton,
+} from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-new',
+  templateUrl: './new.page.html',
+  styleUrls: ['./new.page.scss'],
+  standalone: true,
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonButtons,
+    IonBackButton,
+  ],
+})
+export class NewPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
