42f54b13d6e6a33eb0e9e753f6a527b10881557f
Vitalii Soloninko <130390124+VitaliiSoloninko@users.noreply.github.com>
2026-01-24 22:15:13 +0100

    feat(4 components): create 4 components: home, library, radio, search

---
+++ src/app/library/library-page/library-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-library-page',
+  templateUrl: './library-page.component.html',
+  styleUrls: ['./library-page.component.scss'],
+})
+export class LibraryPageComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
