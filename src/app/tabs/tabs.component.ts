d3920e367d0da37086aa470485868fc6064d7b3c
Vitalii Soloninko <130390124+VitaliiSoloninko@users.noreply.github.com>
2026-02-07 15:37:30 +0100

    feat(firebase): setting Firebase for deployment

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -1,16 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  IonIcon,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
-} from '@ionic/angular/standalone';
+import { IonTabBar, IonTabButton, IonTabs } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.scss'],
-  imports: [IonIcon, IonTabBar, IonTabButton, IonTabs],
+  imports: [IonTabBar, IonTabButton, IonTabs],
 })
 export class TabsComponent implements OnInit {
   constructor() {}
