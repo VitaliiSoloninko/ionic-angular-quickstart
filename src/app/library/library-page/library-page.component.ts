7eb646a6af689729d2b4156196cab359818d65e6
Vitalii Soloninko <130390124+VitaliiSoloninko@users.noreply.github.com>
2026-02-07 15:57:42 +0100

    feat(date and modal): add datetime and modal example

--- src/app/library/library-page/library-page.component.ts
+++ src/app/library/library-page/library-page.component.ts
@@ -4,13 +4,14 @@ import {
   IonHeader,
   IonTitle,
   IonToolbar,
+  IonDatetime,
 } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-library-page',
   templateUrl: './library-page.component.html',
   styleUrls: ['./library-page.component.scss'],
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar],
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonDatetime],
 })
 export class LibraryPageComponent implements OnInit {
   constructor() {}
--- src/app/search/search-page/search-page.component.ts
+++ src/app/search/search-page/search-page.component.ts
@@ -1,19 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import {
+  IonButton,
+  IonButtons,
   IonContent,
   IonHeader,
+  IonInput,
+  IonItem,
+  IonModal,
   IonTitle,
   IonToolbar,
 } from '@ionic/angular/standalone';
+import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
   styleUrls: ['./search-page.component.scss'],
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar],
+  imports: [
+    FormsModule,
+    IonButton,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonInput,
+    IonItem,
+    IonModal,
+    IonTitle,
+    IonToolbar,
+  ],
 })
-export class SearchPageComponent implements OnInit {
-  constructor() {}
+export class SearchPageComponent {
+  @ViewChild(IonModal) modal!: IonModal;
 
-  ngOnInit() {}
+  message =
+    'This modal example uses triggers to automatically open a modal when the button is clicked.';
+  name!: string;
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
+
+  onWillDismiss(event: CustomEvent<OverlayEventDetail>) {
+    if (event.detail.role === 'confirm') {
+      this.message = `Hello, ${event.detail.data}!`;
+    }
+  }
 }
