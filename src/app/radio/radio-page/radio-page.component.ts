551f974cb2f77c74b020d7f022437165a30a029f
Vitalii Soloninko <130390124+VitaliiSoloninko@users.noreply.github.com>
2026-02-07 15:40:55 +0100

    feat(inputs): add input list

--- src/app/radio/radio-page/radio-page.component.ts
+++ src/app/radio/radio-page/radio-page.component.ts
@@ -4,13 +4,24 @@ import {
   IonHeader,
   IonTitle,
   IonToolbar,
+  IonList,
+  IonItem,
+  IonInput,
 } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-radio-page',
   templateUrl: './radio-page.component.html',
   styleUrls: ['./radio-page.component.scss'],
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar],
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonList,
+    IonItem,
+    IonInput,
+  ],
 })
 export class RadioPageComponent implements OnInit {
   constructor() {}
